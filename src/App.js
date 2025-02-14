9779a0b0eb5e53a00283231c86b21a452489f726
yoongyeomp <dbsruaqls123@gmail.com>
2025-02-14 18:21:54 +0900

    25.02.14 폰트 다시 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import NavigationWrapper from './Components/NavigationWrapper';
+import GlobalStyle from './styles/GlobalStyle';
 
 const App = () => {
 
   return (
     <div className="App">
+      <GlobalStyle />
       <BrowserRouter>
         <NavigationWrapper />
       </BrowserRouter>
