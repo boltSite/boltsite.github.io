b95d5fe2846a25ff805209bc1fec1b7ec6328eb6
yoongyeomp <dbsruaqls123@gmail.com>
2025-02-14 18:43:40 +0900

    25.02.14 폰트 import로 부르기

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,4 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
+import Light_woff2 from './fonts/NotoSansKR-Light.woff2';
+import Light_woff from './fonts/NotoSansKR-Light.woff';
+import Light_otf from './fonts/NotoSansKR-Light.otf';
+import Regular_woff2 from './fonts/NotoSansKR-Regular.woff2';
+import Regular_woff from './fonts/NotoSansKR-Regular.woff';
+import Regular_otf from './fonts/NotoSansKR-Regular.otf';
+import Medium_woff2 from './fonts/NotoSansKR-Medium.woff2';
+import Medium_woff from './fonts/NotoSansKR-Medium.woff';
+import Medium_otf from './fonts/NotoSansKR-Medium.otf';
+import Bold_woff2 from './fonts/NotoSansKR-Bold.woff2';
+import Bold_woff from './fonts/NotoSansKR-Bold.woff';
+import Bold_otf from './fonts/NotoSansKR-Bold.otf';
+
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
@@ -6,9 +19,9 @@ const GlobalStyle = createGlobalStyle`
     font-style: normal;
     font-weight: 100;
     font-display: swap;
-    src: url('./styles/fonts/NotoSansKR-Light.woff2') format('woff2'),
-         url('./styles/fonts/NotoSansKR-Light.woff') format('woff'),
-         url('./styles/fonts/NotoSansKR-Light.otf') format('truetype');
+    src: url(${Light_woff2}) format('woff2'),
+         url(${Light_woff}) format('woff'),
+         url(${Light_otf}) format('truetype');
   }
 
   @font-face {
@@ -16,9 +29,9 @@ const GlobalStyle = createGlobalStyle`
     font-style: normal;
     font-weight: normal;
     font-display: swap;
-    src: url('./styles/fonts/NotoSansKR-Regular.woff2') format('woff2'),
-         url('./styles/fonts/NotoSansKR-Regular.woff') format('woff'),
-         url('./styles/fonts/NotoSansKR-Regular.otf') format('truetype');
+    src: url(${Regular_woff2}) format('woff2'),
+         url(${Regular_woff}) format('woff'),
+         url(${Regular_otf}) format('truetype');
   }
 
   @font-face {
@@ -26,9 +39,9 @@ const GlobalStyle = createGlobalStyle`
     font-style: normal;
     font-weight: 500;
     font-display: swap;
-    src: url('./styles/fonts/NotoSansKR-Medium.woff2') format('woff2'),
-         url('./styles/fonts/NotoSansKR-Medium.woff') format('woff'),
-         url('./styles/fonts/NotoSansKR-Medium.otf') format('truetype');
+    src: url(${Medium_woff2}) format('woff2'),
+         url(${Medium_woff}) format('woff'),
+         url(${Medium_otf}) format('truetype');
   }
 
   @font-face {
@@ -36,9 +49,9 @@ const GlobalStyle = createGlobalStyle`
     font-style: normal;
     font-weight: bold;
     font-display: swap;
-    src: url('./styles/fonts/NotoSansKR-Bold.woff2') format('woff2'),
-         url('./styles/fonts/NotoSansKR-Bold.woff') format('woff'),
-         url('./styles/fonts/NotoSansKR-Bold.otf') format('truetype');
+    src: url(${Bold_woff2}) format('woff2'),
+         url(${Bold_woff}) format('woff'),
+         url(${Bold_otf}) format('truetype');
   }
 
   * {
