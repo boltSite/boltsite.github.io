1c40e50d69b81672f29339396423d98a4c15552a
yoongyeomp <dbsruaqls123@gmail.com>
2025-02-20 22:33:13 +0900

    25.02.20 index.js 수정

--- src/index.js
+++ src/index.js
@@ -5,23 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from 'react-helmet-async';
 
-const rootElement = document.getElementById('root');
-const app = (
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
   </React.StrictMode>
 )
-let root = ReactDOM.createRoot(rootElement); 
-
-if (rootElement?.hasChildNodes()) {
-  // 이미 child nodes가 있는 경우, 기존 root를 사용하여 업데이트
-  root.render(app);
-} else {
-  // child nodes가 없는 경우, root를 render로 초기화
-  root.render(app);
-}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
